427c75a6615339d46f7145aa38d3f288895b125a
Kupik <kabutaistrate@gmail.com>
2024-09-08 18:49:03 -0700

    Add asets gameassets

---
+++ Assets/FPS_Survival_Scifi_game_assets/Pack_assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 4f;
+
+    private CharacterController characterController;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        characterController.SimpleMove(transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"))) * speed);
+    }
+}
---
+++ Assets/FPS_Survival_Scifi_game_assets/Pack_assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    public UnityEvent onPlayerEnter;
+
+    public UnityEvent onPlayerExit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")) onPlayerEnter.Invoke();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) onPlayerExit.Invoke();
+    }
+}
